cafd264e2eb6460fce08c652e667fec38b453536
Quentin Correia <481246@student.saxion.nl>
2021-04-20 03:38:07 +0200

    Forgot to change a function call, oop

--- src/commands/language.js
+++ src/commands/language.js
@@ -2,18 +2,18 @@ const database = require('../utils/database');
 const strings = require('../utils/strings');
 
 async function language(msg, [lang]) {
-    const locale = database.get(msg.guild.id, 'settings.language');
     if (!lang) {
-        const string = strings.getString(locale, 'language_current');
+        const locale = database.get(msg.guild.id, 'settings.language');
+        const string = strings.getString(msg.guild.id, 'language_current');
         await msg.channel.send(string.format(locale));
     } else {
         lang = strings.find(lang);
         if (lang) {
             database.set(msg.guild.id, lang, 'settings.language');
-            const string = strings.getString(lang, 'language_changed');
+            const string = strings.getString(msg.guild.id, 'language_changed');
             await msg.channel.send(string.format(lang));
         } else {
-            const string = strings.getString(locale, 'language_not_found');
+            const string = strings.getString(msg.guild.id, 'language_not_found');
             await msg.channel.send(string.format(lang));
         }
     }
