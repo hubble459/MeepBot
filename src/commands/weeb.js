4bf1de44082142c2b455929f211c416bef9b734a
Quentin Correia <481246@student.saxion.nl>
2021-04-25 01:07:46 +0200

    Finished weeb command

--- src/commands/weeb.js
+++ src/commands/weeb.js
@@ -1,19 +1,41 @@
 const fetch = require('node-fetch');
 const {MessageAttachment} = require('discord.js');
+const {getString} = require('../utils/strings');
 
-async function weeb(msg) {
-    let notFound = true;
-    let link = 'https://cdn.awwni.me/n${random}.jpg';
-    while (notFound) {
-        const random = Array(3).fill(0).map(() => {
-            const r = Math.floor(Math.random() * 36);
-            return r > 26 ? r : String.fromCharCode(r + 97);
-        }).join('');
-        link = `https://cdn.awwni.me/n${random}.jpg`;
-        const res = await fetch(link, {method: 'HEAD'});
-        notFound = !res.ok;
+async function weeb(msg, args) {
+    args.unshift('-source:twitter.com');
+
+    const url = 'https://danbooru.donmai.us/posts/random?tags=' + args.join(' ');
+    console.log(url);
+
+    const retries = 3;
+    for (let i = 0; i < retries; i++) {
+        const response = await fetch(url, {method: 'GET', headers: {Accept: 'application/json'}});
+        const json = await response.json();
+        if (response.ok && json.success !== false) {
+            const link = json['large_file_url'] || json['source'];
+            if (link.startsWith('http')) {
+                try {
+                    const res = await fetch(link);
+                    if (res.ok) {
+                        console.log(link);
+                        await msg.channel.send(new MessageAttachment(link));
+                        break;
+                    }
+                } catch (e) {
+                    await msg.channel.send(e.message);
+                    break;
+                }
+            }
+        } else {
+            const str = getString(msg.guild.id, 'weeb_bad_tag');
+            await msg.channel.send(str);
+            break;
+        }
+        if (i === retries - 1) {
+            await msg.channel.send(getString(msg.guild.id, 'weeb_not_found'));
+        }
     }
-    await msg.channel.send(new MessageAttachment(link));
 }
 
 module.exports = {
