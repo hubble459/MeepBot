0300b9f1331fc1529678c80ac8bdbaed406449af
Quentin Correia <481246@student.saxion.nl>
2021-04-25 01:26:13 +0200

    updated weeb

--- src/commands/weeb.js
+++ src/commands/weeb.js
@@ -1,40 +1,33 @@
 const fetch = require('node-fetch');
 const {MessageAttachment} = require('discord.js');
-const {getString} = require('../utils/strings');
+const {getGetString} = require('../utils/strings');
 
 async function weeb(msg, args) {
-    args.unshift('-source:twitter.com');
+    const getString = getGetString(msg.guild.id);
+    // args.unshift('-source:twitter.com');
+    if (args.length > 2) {
+        return msg.channel.send(getString('weeb_too_many_tags'));
+    }
 
-    const url = 'https://danbooru.donmai.us/posts/random?tags=' + args.join(' ');
-    console.log(url);
+    const url = 'https://danbooru.donmai.us/posts/random?tags=' + args.join('%20');
 
-    const retries = 3;
-    for (let i = 0; i < retries; i++) {
-        const response = await fetch(url, {method: 'GET', headers: {Accept: 'application/json'}});
-        const json = await response.json();
-        if (response.ok && json.success !== false) {
-            const link = json['large_file_url'] || json['source'];
-            if (link.startsWith('http')) {
-                try {
-                    const res = await fetch(link);
-                    if (res.ok) {
-                        console.log(link);
-                        await msg.channel.send(new MessageAttachment(link));
-                        break;
-                    }
-                } catch (e) {
-                    await msg.channel.send(e.message);
-                    break;
+    const response = await fetch(url, {method: 'GET', headers: {Accept: 'application/json'}});
+    const json = await response.json();
+    if (response.ok && json.success !== false) {
+        const link = json['large_file_url'] || json['source'];
+        if (link.startsWith('http')) {
+            try {
+                const res = await fetch(link);
+                if (res.ok) {
+                    console.log(link);
+                    await msg.channel.send(new MessageAttachment(link));
                 }
+            } catch (e) {
+                await msg.channel.send(e.message);
             }
-        } else {
-            const str = getString(msg.guild.id, 'weeb_bad_tag');
-            await msg.channel.send(str);
-            break;
         }
-        if (i === retries - 1) {
-            await msg.channel.send(getString(msg.guild.id, 'weeb_not_found'));
-        }
+    } else {
+        await msg.channel.send(getString('weeb_not_found'));
     }
 }
 
